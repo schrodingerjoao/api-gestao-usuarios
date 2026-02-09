c5709b8b8383ebd0525959683a5e848cd2f2aea3
João Paulo <fagundesjoao1902@outlook.com>
2026-02-08 22:28:35 -0300

    Add UsuarioNaoEncontradoException class

---
+++ src/main/java/com/example/usermanagement/exception/UsuarioNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.example.usermanagement.exception;
+
+public class UsuarioNaoEncontradoException extends RuntimeException {
+    public UsuarioNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
